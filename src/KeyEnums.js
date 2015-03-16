1eaed79f1b43cb6c39d673d186483c7a45f90df3
kbirk <birk.kevin@gmail.com>
2015-03-16 00:49:07 -0400

    Added missing file.

---
+++ src/KeyEnums.js
@@ -0,0 +1,35 @@
+(function () {
+
+    "use strict";
+
+    module.exports = {
+
+        BACKSPACE: 'backspace',
+        TAB: 'tab',
+        ENTER: 'enter',
+        SHIFT: 'shift',
+        CTRL: 'ctrl',
+        ALT: 'alt',
+        PAUSE_BREAK: 'pausebreak',
+        CAPS_LOCK: 'capslock',
+        ESC: 'esc',
+        PAGE_UP: 'pageup',
+        PAGE_DOWN: 'pagedown',
+        END: 'end',
+        HOME: 'home',
+        LEFT_ARROW: 'left',
+        UP_ARROW: 'up',
+        RIGHT_ARROW: 'right',
+        DOWN_ARROW: 'down',
+        PRINT_SCREEN: 'printscreen',
+        INSERT: 'insert',
+        DELETE: 'delete',
+        WINDOWS: 'windows',
+        SELECT: 'select',
+        SPACE_BAR: 'space',
+        NUM_LOCK: 'numlock',
+        SCROLL_LOCK: 'scrolllock',
+
+    };
+
+}());
